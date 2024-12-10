2cc05c4c03da49a13860751609d7a91036b39846
AadilDiwan107 <aadildiwan107@gmail.com>
2024-12-10 18:57:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Contact Form Validation Example
+document.querySelector("#contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you for reaching out! We will get back to you shortly.");
+});
